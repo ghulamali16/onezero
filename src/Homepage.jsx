c89b92b40725d1d4b79193f89843a8c2a391dec4
ghulamali16 <ghulam.ali@devsinc.com>
2021-12-06 23:39:35 +0500

    Refactored Homepage Code

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,7 +4,7 @@ import ProductCard from "./ProductCard";
 import { useState, useEffect } from "react";
 import Banner from "./Banner";
 const Homepage = () => {
-  const [productsArr, setProductsArr] = useState([2, 5, 6]);
+  const [productsArr, setProductsArr] = useState([2, 5, 6, 8, 10]);
   // useEffect(() => {
   //   let res = axios.get("http://localhost:8000/core/products/?offset=0");
   //   setProductsArr(res);
